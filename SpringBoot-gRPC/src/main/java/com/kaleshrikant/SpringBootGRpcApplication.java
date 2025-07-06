b8c07f7bbd5bf3bf0aa9d3033e9d88a41463bef4
Shrikant Kale <kaleshrikant@live.com>
2025-07-06 22:31:41 +0530

    Initial Commit

---
+++ SpringBoot-gRPC/src/main/java/com/kaleshrikant/SpringBootGRpcApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGRpcApplication.class, args);
+	}
+
+}
